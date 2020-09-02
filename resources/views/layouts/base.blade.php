5b6f74914cedabec367703daf5deb3b9d1998594
佐々木智美 <t.m1ss0.do@gmail.com>
2020-09-02 11:39:16 +0900

    HTML&CSS ( header . footer ) / footerの固定

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,39 +13,83 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <!-- <link rel="dns-prefetch" href="//fonts.gstatic.com"> -->
-    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
+
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  <header>
-
-    @if (Route::has('login'))
-        <div class="">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+  <header class="l-header">
+    <div class="c-header__container">
+      <h1 class="c-title__header-logo">
+        <a class="c-title__header-link" href="#">match</a>
+      </h1>
+      <nav class="c-nav__right">
+          @if (Route::has('login'))
+            <div class="c-nav__container">
+              @auth
+              <!-- ログインしていた場合 -->
+              <a class="c-nav__item--top" href="{{ url('/home') }}">Home</a>
+              <a class="c-nav__item--top" href="#">案件を探す</a>
+              <a class="c-nav__item--top" href="#">マイページ</a>
+              <a class="c-nav__item--top" href="#">依頼する</a>
+              @else
+              <!-- ゲストの場合 -->
+              <a class="c-nav__item--top" href="#">案件を見てみる</a>
+              <a class="c-nav__item--top" href="{{ route('login') }}">Login</a>
 
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
+                <a class="c-nav__item--top" href="{{ route('register') }}">Register</a>
                 @endif
-            @endauth
+
+              @endauth
+            </div>
+          @endif
+      </nav>
+    </div>
+    @if (session('status'))
+        <div class="c-session-msg">
+            {{ session('status') }}
+            例文メッセージです：登録しました！
         </div>
     @endif
-
   </header>
 
-  @if (session('status'))
-      <div class="">
-          {{ session('status') }}
-      </div>
-  @endif
 
-  <main class="">
-    @yield('content')
+
+  <main class="l-main">
+    <div class="l-main__wrap">
+      @yield('content')
+    </div>
   </main>
+  <footer class="l-footer">
+    <nav class="c-nav">
+        @if (Route::has('login'))
+          <div class="c-nav__container">
+            @auth
+            <!-- ログインしていた場合 -->
+            <a class="c-nav__item--small" href="{{ url('/home') }}">Home</a>
+            <a class="c-nav__item--small" href="#">案件を探す</a>
+            <a class="c-nav__item--small" href="#">マイページ</a>
+            <a class="c-nav__item--small" href="#">依頼する</a>
+            @else
+            <!-- ゲストの場合 -->
+            <a class="c-nav__item--small" href="#">案件を見てみる</a>
+            <a class="c-nav__item--small" href="{{ route('login') }}">Login</a>
+
+              @if (Route::has('register'))
+              <a class="c-nav__item--small" href="{{ route('register') }}">Register</a>
+              @endif
+
+            @endauth
+          </div>
+        @endif
+    </nav>
+    <p class="text-align__center font__ss" >©︎webukatu,created by Tomomi Sasaki</p>
+  </footer>
 </body>
 </html>
--- resources/views/projects/all.blade.php
+++ resources/views/projects/all.blade.php
@@ -8,4 +8,8 @@ project indexアクション
 案件一覧ページです。
 Homeでもあります。
 
+<h3 class="c-title__sub">sub fontです。みんなはっぴー！</h3>
+<p class="c-title__sub">sub fontです。みんなはっぴー！</p>
+
+
 @endsection
