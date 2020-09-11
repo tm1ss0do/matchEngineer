1dbe548122432b5604ee99ae118bafaad29882ed
佐々木智美 <t.m1ss0.do@gmail.com>
2020-09-11 16:41:50 +0900

    projectモデル作成

---
+++ app/Project.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //
+     protected $fillable = ['project_title', 'project_status', 'project_type', 'project_reception_end', 'project_max_amount', 'project_mini_amount', 'project_detail_desc'];
+}
