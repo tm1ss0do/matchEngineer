16cceebbacf303577dc003adb5f35d459a0bd949
佐々木智美 <t.m1ss0.do@gmail.com>
2020-09-16 13:19:35 +0900

    不要なファイル削除（counter.js）

--- resources/js/counter.js
+++
@@ -1,9 +0,0 @@
-let count_area = $('.js-count-area');
-let counter = $('.js-counter');
-
-count_area.oninput = handleInput;
-
-function handleInput(e) {
-  counter.textContent = `フィールドの value は
-      ${e.target.value.length} 文字の長さです。`;
-}
