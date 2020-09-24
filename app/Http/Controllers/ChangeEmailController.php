b20daab53fb097dd427d94a1d91014bd13886349
佐々木智美 <t.m1ss0.do@gmail.com>
2020-09-25 07:54:50 +0900

    EmailReset用の新規ファイル

---
+++ app/Http/Controllers/ChangeEmailController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Project;
+use App\PublicMsg;
+use App\DirectMsgsBoard;
+use App\DirectMsgs;
+use App\PublicNotify;
+use App\DirectNotify;
+use App\EmailReset;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreProjectPost;
+use App\Http\Requests\StoreMessageRequest;
+use App\Http\Requests\StoreProfileRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ChangeEmailController extends Controller
+{
+    //
+    public function email_edit_post(Request $request, $id){
+      // 新しいメールアドレスの使用を確認してから、メールアドレスを更新する処理
+
+      $new_email = $request->new_email;
+
+        // トークン生成
+        $token = hash_hmac(
+            'sha256',
+            Str::random(40) . $new_email,
+            config('app.key')
+        );
+
+        // トークンをDBに保存
+        DB::beginTransaction();
+        try {
+            $param = [];
+            $param['user_id'] = $id;
+            $param['new_email'] = $new_email;
+            $param['token'] = $token;
+            $email_reset = EmailReset::create($param);
+
+            DB::commit();
+
+            $email_reset->sendEmailResetNotification($token);
+
+            return redirect('/home')->with('flash_message', '確認メールを送信しました。');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect('/home')->with('flash_message', 'メール更新に失敗しました。');
+        }
+
+    }
+
+    public function email_reset(Request $request, $token){
+
+      $email_resets = DB::table('email_resets')
+                      ->where('token', $token)
+                      ->first();
+
+      // トークンが存在している、かつ、有効期限が切れていないかチェック
+      if ($email_resets && !$this->tokenExpired($email_resets->created_at)) {
+
+        // ユーザーのメールアドレスを更新
+        $user = User::find($email_resets->user_id);
+        $user->email = $email_resets->new_email;
+        $user->save();
+
+        // レコードを削除
+        DB::table('email_resets')
+        ->where('token', $token)
+        ->delete();
+
+        return redirect('/home')->with('flash_message', 'メールアドレスを更新しました！');
+      }
+      else {
+        // レコードが存在していた場合削除
+        if ($email_resets) {
+          DB::table('email_resets')
+          ->where('token', $token)
+          ->delete();
+        }
+        return redirect('/home')->with('flash_message', 'メールアドレスの更新に失敗しました。');
+      }
+
+    }
+
+    protected function tokenExpired($createdAt)
+    {
+      // トークンの有効期限は60分に設定
+      $expires = 60 * 60;
+      return Carbon::parse($createdAt)->addSeconds($expires)->isPast();
+    }
+}
