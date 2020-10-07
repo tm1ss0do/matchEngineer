e31b5fd46665606282f3f426e6cb0b67b25b821d
佐々木智美 <t.m1ss0.do@gmail.com>
2020-10-07 13:02:57 +0900

    メールテキスト用のテンプレートをエクスポートしました。

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td>
+<table class="" align="" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="" align="">
+{{ Illuminate\Mail\Markdown::parse($slot) }}
+</td>
+</tr>
+</table>
+</td>
+</tr>
