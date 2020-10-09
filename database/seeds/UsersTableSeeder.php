ea6dbccad92c2279a8d32d8f9922ef52c5cca18f
佐々木智美 <t.m1ss0.do@gmail.com>
2020-10-10 01:55:44 +0900

    master

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -30,7 +30,6 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'test2',
             'email' => 'test2@example.com',
-            'delete_flg' => '0',
             'email_verified_at' => now(),
             'profile_icon' => NULL,
             'password' => bcrypt('pas2word'),
@@ -44,24 +43,17 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'test3',
             'email' => 'test3@example.com',
-            'delete_flg' => '0',
-            'self_introduction'=> "自己紹介文3
+            'email_verified_at' => now(),
+            'profile_icon' => NULL,
+            'password' => bcrypt('pas3word'),
+            'self_introduction'=> "自己紹介文333
             ここに自己紹介文を入れます。
             （勤務した会社・勤務年数・所有資格・扱っている言語など）",
-            'password' => bcrypt('pas3word'),
+            'deleted_at' => NULL,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
-        DB::table('users')->insert([
-            'name' => 'test4',
-            'email' => 'test4@example.com',
-            'delete_flg' => '1',
-            'self_introduction'=> "自己紹介文4
-            削除されているはずのユーザーです",
-            'password' => bcrypt('pas4word'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+
 
         //factoryを使ってダミーデータ生成
         factory(App\User::class, 10)->create();
